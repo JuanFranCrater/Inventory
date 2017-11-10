594f06f2b2bc0be0dbb8afe7b39c394b1aeaa873
JuanFCrater <juanfbenitezlopez@gmail.com>
2017-11-10 12:58:35 +0100

    Pruebas

--- app/src/androidTest/java/com/example/inventory/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/inventory/ExampleInstrumentedTest.java
@@ -1,12 +1,25 @@
 package com.example.inventory;
 
 import android.content.Context;
+import android.support.annotation.StringRes;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
+
+import com.example.inventorymaterial.R;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -32,32 +45,46 @@ public class ExampleInstrumentedTest {
     //Caso de prueba que comprueba que el campo Usuario no esta vacío
     @Test
     public void isUserEmpty(){
-
+        onView(withId(R.id.btnSignIN)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorUserEmpty);
     }
 
-    //Caso de prueba que comprueba que el campo Email no esta vacío
+    //Caso de prueba que comprueba que el campo Email no esta
+    // vacío
     @Test
     public void isEmailEmpty() {
-
+        onView(withId(R.id.btnSignIN)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorEmailEmpty);
     }
 
     //Caso de prueba que comprueba que el campo Password no esta vacío
     @Test
     public void isPasswordEmpty() {
-
+        onView(withId(R.id.btnSignIN)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordEmpty);
     }
+
+
 
     /**
      * Caso de prueba que comprueba la longitud de la contraseña
      */
     @Test
     public void passwordLength() {
-
+        onView(withId(R.id.edtUser)).perform(typeText("lourdes"),closeSoftKeyboard());
+        onView(withId(R.id.edtPassword)).perform(typeText("lu"),closeSoftKeyboard());
+        onView(withId(R.id.btnSignIN)).perform(click());
+        checkSnackBarDisplayByMessage(R.string.errorPasswordEmpty);
     }
 
     @Test
     public void passwordDoubleCheck () {
 
     }
+    private void checkSnackBarDisplayByMessage(@StringRes int message) {
+        onView(withText(message)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 
     }
+
+    }
+
