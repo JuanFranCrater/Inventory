783b47de516a9aec014668d73d92c999f7b1dd3c
JuanFranCrater <juanfbenitezlopez@gmail.com>
2018-01-31 17:50:44 +0100

    Contrat actualizado con lo que Lourdes quiere

--- app/src/main/java/com/example/inventorymaterial/data/db/InventoryContract.java
+++ app/src/main/java/com/example/inventorymaterial/data/db/InventoryContract.java
@@ -18,6 +18,89 @@ public final class InventoryContract {
     public static final int DATABASE_VERSION=5;
     public static final String DATABASE_NAME="Inventory.db";
 
+    public static class ProductEntry implements BaseColumns {
+        public static final String TABLE_NAME = "product";
+
+        public static final String COLUMN_PRODUCTID = "id";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_CATEGORY = "category";
+        public static final String COLUMN_SUBCATEGORY = "subcategory";
+        public static final String COLUMN_CLASS = "class";
+        public static final String COLUMN_TIPO = "type";
+        public static final String COLUMN_SECTIONID = "section";
+        public static final String COLUMN_DEPENDENCYID = "sectionid";
+        public static final String COLUMN_INVENTORYID = "inventoryid";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_SERIAL = "serial";
+        public static final String COLUMN_CANTIDAD = "cantidad";
+        public static final String COLUMN_VALUE = "value";
+        public static final String COLUMN_VENDOR = "vendor";
+        public static final String COLUMN_URL = "url";
+        public static final String COLUMN_DATEPURCHASE = "datepurchase";
+        public static final String COLUMN_NOTES = "notes";
+
+        public static final String[] ALL_COLUMNS = new String[] {
+                BaseColumns._ID, COLUMN_DESCRIPTION, COLUMN_DEPENDENCYID, COLUMN_SECTIONID, COLUMN_CATEGORY, COLUMN_TIPO, COLUMN_SECTIONID
+        };
+
+        public static final String SQL_CREATE_ENTRIES = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " + "%s TEXT NOT NULL, " + "%s INTEGER NOT NULL, " +
+                        "%s INTEGER NOT NULL, " + "%s INTEGER NOT NULL, " + "%s INTEGER NOT NULL, " +
+                        "FOREIGN KEY (%s) REFERENCES %s(%s) ON UPDATE CASCADE ON DELETE RESTRICT)" +
+                        "FOREIGN KEY (%s) REFERENCES %s(%s) ON UPDATE CASCADE ON DELETE RESTRICT)",
+                TABLE_NAME,
+                BaseColumns._ID,
+                COLUMN_DESCRIPTION,
+                COLUMN_DEPENDENCYID,
+                COLUMN_SECTIONID,
+                COLUMN_CATEGORY,
+                COLUMN_TIPO,
+                COLUMN_SECTIONID,
+                SectorEntry.TABLE_NAME,
+                BaseColumns._ID,
+                COLUMN_DEPENDENCYID,
+                DependencyEntry.TABLE_NAME,
+                BaseColumns._ID);
+
+        public static final String SQL_INSERT_ENTRIES = String.format("INSERT INTO %s (%s, %s, %s, %s, %s) VALUES (%s, '%s', '%s', '%s', '%s'), (%s, '%s', '%s', '%s', '%s'), (%s, '%s', '%s', '%s', '%s')",
+                TABLE_NAME, COLUMN_DESCRIPTION, COLUMN_DEPENDENCYID, COLUMN_SECTIONID, COLUMN_CATEGORY, COLUMN_TIPO,
+                "ProductoA", "1", "1", "1", "1",
+                "ProductoB", "2", "2", "2", "2",
+                "ProductoC", "3", "3", "3", "3");
+
+        public static final String SQL_DELETE_ENTRIES = String.format("DROP TABLE IF EXISTS %s", TABLE_NAME);
+
+    }
+
+    public static class CategoryEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "categoria";
+        public static final String COLUMN_NAME = "nombre";
+
+        public static final String SQL_CREATE_ENTRIES = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " + "%s TEXT NOT NULL)", TABLE_NAME, BaseColumns._ID, COLUMN_NAME);
+
+        public static final String SQL_INSERT_ENTRIES = String.format("INSERT INTO %s (%s) VALUES ('%s'), ('%s'), ('%s')", TABLE_NAME, COLUMN_NAME, "CategoríaA", "CategoríaB", "CategoríaC");
+
+    }
+
+    public static class TipoEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "type";
+        public static final String COLUMN_NAME = "nombre";
+
+        public static final String SQL_CREATE_ENTRIES = String.format("CREATE TABLE %s (%s INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        "%s TEXT NOT NULL)",
+                TABLE_NAME,
+                BaseColumns._ID,
+                COLUMN_NAME);
+
+        public static final String SQL_INSERT_ENTRIES = String.format("INSERT INTO %s (%s) VALUES ('%s'), ('%s'), ('%s')",
+                TABLE_NAME,
+                COLUMN_NAME,
+                "Tipo1", "Tipo2", "Tipo3");
+
+    }
+
+
     // Por cada tabla se crea una clase que implementa la interfaz BaseColumns
     public static class DependencyEntry implements BaseColumns {
         public static final String TABLE_NAME = "dependency";
